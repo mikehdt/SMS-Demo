494ed9b7376779958071bc7b92d97c49e59bb122
Mike Hopkins <mike@dthought.net>
2021-12-15 14:44:05 +1100

    Remove compiled assets after converting to banks

--- scripts/assets.bat
+++ scripts/assets.bat
@@ -9,11 +9,16 @@ cd assets
 :: Backgrounds
 for %%f in (.\backgrounds\*.png) do ..\utils\bmp2tile\BMP2Tile.exe "%%f" -mirror -removedupes -palsms -savetiles "%%~nf.tiles.psgcompr" -savetilemap "%%~nf.tilemap.stmcompr" -savepalette "%%~nf.palette.bin"
 
-:: Convert to ROM banks
-cd ..\compile
-assets2banks ..\assets --singleheader --compile
+:: Convert to ROM banks and move them to the compile folder for use
+assets2banks . --singleheader --compile
+move *.rel ..\compile
 
 :: This header is needed for the source to build
 move assets2banks.h ..\src
+
+:: Tidy up
+del *.bin > nul
+del *.stmcompr > nul
+del *.psgcompr > nul
 
 cd ..
